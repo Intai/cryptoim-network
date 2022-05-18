1c645cb2b592600fbc081888729a87971cf200b4
intai <intai.hg@gmail.com>
2022-05-18 23:15:40 +1200

    Rename copy.svg to clipboard

--- app/components/invite.jsx
+++ app/components/invite.jsx
@@ -167,7 +167,7 @@ const Invite = (props) => {
             </InviteAnchor>
             <CopyContainer>
               <CopyIcon
-                src={getStaticUrl('/icons/copy.svg')}
+                src={getStaticUrl('/icons/clipboard.svg')}
                 title="Copy your invite link"
                 onClick={handleCopy}
               />
